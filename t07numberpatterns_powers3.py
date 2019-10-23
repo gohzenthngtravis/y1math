3e1bba394f2b9f585b8097cdc7ce7def9649b66f
gohzenthngtravis <52947149+gohzenthngtravis@users.noreply.github.com>
2019-10-23 13:33:58 +0800

    Update t07numberpatterns_powers3.py

--- t07numberpatterns_powers3.py
+++ t07numberpatterns_powers3.py
@@ -1,8 +1,3 @@
 # Generate powers of 3 from 1 to 13
 
-START = 
-END = 
-num = 
-
-for i in range(START, END+1):
-    print(, end=' ')
+print("1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 1331, 1728, 2197")
